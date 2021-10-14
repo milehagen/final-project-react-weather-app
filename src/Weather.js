f0025a96fa264479454f0d2283fa86d9f5711d2b
Mi <74912354+milehagen@users.noreply.github.com>
2021-10-14 22:37:02 +0200

    added search box and button, and border around app.

--- src/Weather.js
+++ src/Weather.js
@@ -3,9 +3,21 @@ import "./Weather.css";
 
 export default function Weather(){
     return (
+        
         <div className="weather">
-<div className="row">
+       
+      <form>  
+          <div className="row">
+          <div className="col-6">
+                <input type="text" placeholder="Enter a city..." className="form-control" autoFocus="yes"/>
+            </div>
+                 <div className="col-3">
+                <button type="button" className="btn btn-primary  w-100">Search</button>
+            </div>
+         </div>
+    </form>
 
+<div className="row">
             <div className="col-6 current-weather-temp">
             <img className="current-weather-icon"src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" alt="current-weather-pic"/> 
             <div className="temperature">
@@ -17,22 +29,27 @@ export default function Weather(){
                   <div>Precipitation:<span>84%</span></div>
                     <div>Humidity:<span>81%</span></div>
                     <div>Wind: <span>5 m/s</span></div>
-                    </div>
             </div>
-          
-            </div>
-           
+        </div>
+    </div>
+</div>
 
-
-                </div>
   <div className="col-6 city-date-description">
         <div className="current-weather-description">
             <h3>Oslo</h3>
                 <div className="day">Thursday 13:18</div>
                     <div className="weather-description"><span>Light rain showers</span></div>
         </div>
-        </div>
-      </div>
-      </div>
+    </div>
+</div>
+<hr/>
+<div className="row">
+<div className="col-2">
+    <p>test</p>
+
+</div>
+
+</div>
+</div>
     )
 }
