86a0eb068e749638a9dbeff675c335c2abe39062
Mi <74912354+milehagen@users.noreply.github.com>
2021-10-14 21:32:50 +0200

    centered the weather app

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,38 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather(){
     return (
-        <div>
-            <p> <img src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"/> 9°C</p>
-            <span>
-            <small>
-                <ul>
-                    <li>Precipitation</li>
-                    <li>Humidity</li>
-                    <li>Wind</li>
-                    
-                </ul>
-            </small>
-            </span>
+        <div className="weather">
+<div className="row">
+
+            <div className="col-6 current-weather-temp">
+            <img className="current-weather-icon"src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" alt="current-weather-pic"/> 
+            <div className="temperature">
+            <div className="temperature-metric-controller">
+                <span className="current-temperature">9</span>
+            <div className="metric-controller">°C |°F</div>
+
+            <div className="precip-humid-wind">
+                  <div>Precipitation:<span>84%</span></div>
+                    <div>Humidity:<span>81%</span></div>
+                    <div>Wind: <span>5 m/s</span></div>
+                    </div>
+            </div>
+          
+            </div>
+           
+
+
+                </div>
+  <div className="col-6 city-date-description">
+        <div className="current-weather-description">
+            <h3>Oslo</h3>
+                <div className="day">Thursday 13:18</div>
+                    <div className="weather-description"><span>Light rain showers</span></div>
         </div>
+        </div>
+      </div>
+      </div>
     )
 }
