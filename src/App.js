d30c772c41591a18e8c62710b27aba7b9ee01a67
Mi <74912354+milehagen@users.noreply.github.com>
2021-10-14 11:25:50 +0200

    added small text and bootstrap container

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "bootstrap/dist/css/bootstrap.css";
  export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Hello World</h1>
+      <small>Coded by <a href="https://www.linkedin.com/in/mi-le-hagen-5aa0b617/" target="_blank">Mi Le Hagen</a></small>
+      </div>
     </div>
   );
 }
